b170c4202fea5b5a980623e2d93167794ab49bfe
Ilya Semenov <deff3010@gmail.com>
2024-09-26 19:56:56 +0300

    Added navigation

--- ClientApp/src/app/components/portal/portal-layout/portal-layout.component.ts
+++ ClientApp/src/app/components/portal/portal-layout/portal-layout.component.ts
@@ -1,17 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-portal-layout',
   standalone: true,
-  imports: [MatToolbarModule, MatSidenavModule, MatListModule, RouterOutlet],
+  imports: [MatToolbarModule, MatSidenavModule, MatListModule, RouterOutlet, RouterModule],
   templateUrl: './portal-layout.component.html',
   styleUrl: './portal-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PortalLayoutComponent {
-  readonly links = ['Home', 'My Games', 'Find party', 'Settings', 'Privacy policy']
+
 }
