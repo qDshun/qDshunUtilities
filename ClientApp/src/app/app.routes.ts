2e3c010c562971d063605dc74a07c2e56da752fc
--replace-all <aboudzhelida@gmail.com>
2024-09-26 16:48:37 +0200

    initial implementation of world screen UI

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { WorldSelectComponent } from './components/loot-tables/world-select/world-select.component';
 import { authGuard } from './guards';
+import { WorldSelectComponent } from './components/world-select/world-select.component';
+import { WorldComponent } from './components/world/world.component';
 
 export const routes: Routes = [
   {
@@ -18,13 +19,19 @@ export const routes: Routes = [
   {
     path: '',
     component: WorldSelectComponent,
-    title: 'Worlds',
+    title: 'WorldSelect',
     canActivate: [authGuard]
   },
   {
-    path: 'worlds',
+    path: 'WorldSelect',
     component: WorldSelectComponent,
-    title: 'Worlds',
+    title: 'WorldSelect',
+    canActivate: [authGuard]
+  },
+  {
+    path: 'World/:worldId',
+    component: WorldComponent,
+    title: 'WorldUI',
     canActivate: [authGuard]
   },
 ];
