ee8a75a4d757b769ee766f59745cd6092e363c7b
Ilya Semenov <deff3010@gmail.com>
2024-10-09 16:52:40 +0300

    Game module improvements

--- ClientApp/src/app/components/game/game-bar-quick-access/game-bar-quick-access.component.ts
+++ ClientApp/src/app/components/game/game-bar-quick-access/game-bar-quick-access.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { WorldObjectListComponent } from '../world-object-list/world-object-list.component';
 
 @Component({
   selector: 'app-game-bar-quick-access',
   standalone: true,
-  imports: [],
+  imports: [WorldObjectListComponent],
   templateUrl: './game-bar-quick-access.component.html',
   styleUrl: './game-bar-quick-access.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GameBarQuickAccessComponent {
-
+  // TO BE DISCUSSED
 }
